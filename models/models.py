58925812e0ec289ccd021c39cf3b1701c161f104
Charles <charlesvernerey2@gmail.com>
2018-05-01 11:34:13 +0200

    ajout d'attributs dans les modèles cours et session + mise à jour de la vue associée

--- models/models.py
+++ models/models.py
@@ -7,6 +7,7 @@ class Course(models.Model):
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
+    responsible_id = fields.Many2one('res.users', ondelte='set null', string='Responsible', index=True)
 
 class Session(models.Model):
     _name = 'openacademy.session'
@@ -15,4 +16,6 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digits=(6,2), help="duration in days")
     seats = fields.Integer(string="number of seats")
+    instructor_id = fields.Many2one('res.partner', string='instructor')
+    course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
 
