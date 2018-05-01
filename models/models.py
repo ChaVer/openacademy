f10429c78545c9f6c0705f08b5848aa75c15306f
Charles <charlesvernerey2@gmail.com>
2018-05-01 12:16:11 +0200

    Ajout de la possibilité d'ajouter plusieurs participants à une session

--- models/models.py
+++ models/models.py
@@ -19,4 +19,5 @@ class Session(models.Model):
     seats = fields.Integer(string="number of seats")
     instructor_id = fields.Many2one('res.partner', string='instructor')
     course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
 
